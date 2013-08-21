6ab8d0bc9d18f71b2b30c8fa1edba9d71186877f
Tomas Radej <tradej@redhat.com>
2013-08-21 11:48:38 +0200

    Homepage template

--- website.py
+++ website.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 app.debug = True
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
